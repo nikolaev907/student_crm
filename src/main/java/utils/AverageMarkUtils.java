e33b931c16310a66b335d9e5691af84c28067184
NikolaevAB <nikolaev1972.72@mail.ru>
2020-09-30 09:16:48 +0300

    Round up to two decimal places
    Round up to two decimal places for correct output average mark, in
    method AverageMarkUtils.getAverageMark().

--- src/main/java/utils/AverageMarkUtils.java
+++ src/main/java/utils/AverageMarkUtils.java
@@ -8,9 +8,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class AverageMarkUtils {
-    public double getAverageMark(Map<String, Object> data) {
+    public double getAverageMark( Map<String, Object> data) {
         List marksId = (ArrayList) data.get("marksId");
         String marksIdStr = (String) marksId.stream().map(Object::toString).collect(Collectors.joining(", "));
-        return DBManager.getAverageGradeByIdMark(marksIdStr);
+        double averageGradeByIdMark = DBManager.getAverageGradeByIdMark(marksIdStr);
+        return Math.round(averageGradeByIdMark * 100.0) / 100.0;
     }
 }
